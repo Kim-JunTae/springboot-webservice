5b42c2c330b7d82d612c55d25b5c81065e0cd2dc
Jun <rardin@naver.com>
2020-11-29 23:20:27 +0900

    [2020.11.29] 3장 완료

---
+++ src/main/java/com/juntae/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.juntae.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
