1b853559864e595645291010517cf25d13d514f0
Jun <rardin@naver.com>
2020-12-05 23:03:45 +0900

    [2020.12.05] 5장 구글 로그인

--- src/main/java/com/juntae/book/springboot/web/IndexController.java
+++ src/main/java/com/juntae/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.juntae.book.springboot.web;
 
+import com.juntae.book.springboot.config.auth.dto.SessionUser;
+import com.juntae.book.springboot.domain.user.User;
 import com.juntae.book.springboot.service.posts.PostsService;
 import com.juntae.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (User) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
